54519b0454888c9feeebf5c32af9b757b25e194a
Dev Emílio <emilio.notb@gmail.com>
2026-03-06 12:47:08 -0300

    Update app.py

--- Dashboard_imobiliaria/app.py
+++ Dashboard_imobiliaria/app.py
@@ -165,8 +165,4 @@ with col_right:
 st.sidebar.markdown("---")
 st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
-st.sidebar.image(
-    "Dashboard_imobiliaria/assets/logo.jpg", use_container_width=True
-)
-
 st.sidebar.markdown("<br>", unsafe_allow_html=True)
